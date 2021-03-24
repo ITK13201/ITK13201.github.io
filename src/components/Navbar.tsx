620e19946453b750004e5d5bf72ebbfecd2e09c3
ITK13201 <57588603+ITK13201@users.noreply.github.com>
2021-03-25 02:59:25 +0900

    modify: responsive design

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react'
 import {
   AppBar,
   Toolbar,
@@ -9,23 +9,92 @@ import {
   makeStyles,
   Box,
   Link,
-} from "@material-ui/core";
+  Menu,
+  MenuItem,
+} from '@material-ui/core'
+import { Menu as MenuIcon } from '@material-ui/icons'
+import MediaQuery from 'react-responsive'
 
 const useStyles = makeStyles((theme) => ({
   navbar: {
-    display: "flex",
-    justifyContent: "space-between",
+    display: 'flex',
+    justifyContent: 'space-between',
   },
   navbarTitle: {
-    padding: "10px",
+    padding: '10px',
   },
-}));
+}))
 
-const Navbar: React.FC = () => {
-  const classes = useStyles();
+const SpNavbarList: React.FC = () => {
+  const [anchorEl, setAnchorEl] = React.useState(null)
+
+  const handleClick = (event: any) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
 
   return (
-    <React.Fragment>
+    <>
+      <Button
+        aria-controls="simple-menu"
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        <MenuIcon />
+      </Button>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>
+          <Link href="#about" color="inherit" underline="none">
+            ABOUT
+          </Link>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Link href="#skills" color="inherit" underline="none">
+            SKILLS
+          </Link>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Link href="#works" color="inherit" underline="none">
+            WORKS
+          </Link>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Link href="#contact" color="inherit" underline="none">
+            CONTACT
+          </Link>
+        </MenuItem>
+      </Menu>
+    </>
+  )
+}
+
+const PcNavbarList: React.FC = () => {
+  return (
+    <>
+      <List>
+        <Button href="#about">ABOUT</Button>
+        <Button href="#skills">SKILLS</Button>
+        <Button href="#works">WORKS</Button>
+        <Button href="#contact">CONTACT</Button>
+      </List>
+    </>
+  )
+}
+
+const Navbar: React.FC = () => {
+  const classes = useStyles()
+
+  return (
+    <>
       <AppBar color="default" position="static">
         <Toolbar>
           <Container maxWidth="md" className={classes.navbar}>
@@ -35,22 +104,22 @@ const Navbar: React.FC = () => {
                 color="initial"
                 className={classes.navbarTitle}
               >
-                <Link href="/" color="inherit">
+                <Link href="/" color="inherit" underline="none">
                   Portfolio
                 </Link>
               </Typography>
             </Box>
-            <List>
-              <Button href="#about">ABOUT</Button>
-              <Button href="#skills">SKILLS</Button>
-              <Button href="#works">WORKS</Button>
-              <Button href="#contact">CONTACT</Button>
-            </List>
+            <MediaQuery query="(max-width: 767px)">
+              <SpNavbarList />
+            </MediaQuery>
+            <MediaQuery query="(min-width: 767px)">
+              <PcNavbarList />
+            </MediaQuery>
           </Container>
         </Toolbar>
       </AppBar>
-    </React.Fragment>
-  );
-};
+    </>
+  )
+}
 
-export default Navbar;
+export default Navbar
