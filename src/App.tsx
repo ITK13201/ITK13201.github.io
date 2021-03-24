e072199950bdc952ce1354b0a69fbe2c8f940577
ITK13201 <57588603+ITK13201@users.noreply.github.com>
2021-03-24 23:27:15 +0900

    add: npm scripts

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React from "react";
+import React from 'react'
 
-import Navbar from "./components/Navbar";
-import "./App.scss";
+import Navbar from './components/Navbar'
+import './App.scss'
 
 const App: React.FC = () => {
   return (
     // Navigation Bar
     <Navbar />
-  );
-};
+  )
+}
 
-export default App;
+export default App
