97281dc016141c8bd503adccf409cf427535b4d8
ITK13201 <57588603+ITK13201@users.noreply.github.com>
2021-03-24 23:09:21 +0900

    modify: formatted by prettier

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,53 +1,56 @@
-import React from 'react'
-import { AppBar, Toolbar, Button, Typography, Container, IconButton, List, makeStyles, Box, Link } from '@material-ui/core'
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  Button,
+  Typography,
+  Container,
+  List,
+  makeStyles,
+  Box,
+  Link,
+} from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   navbar: {
-    display: 'flex',
-    justifyContent: 'space-between'
+    display: "flex",
+    justifyContent: "space-between",
   },
   navbarTitle: {
-    padding: "10px"
-  }
-}))
+    padding: "10px",
+  },
+}));
 
 const Navbar: React.FC = () => {
   const classes = useStyles();
 
   return (
     <React.Fragment>
-      <AppBar
-        color='default'
-        position='static'
-      >
+      <AppBar color="default" position="static">
         <Toolbar>
           <Container maxWidth="md" className={classes.navbar}>
             <Box>
-              <Typography variant="h5" color="initial" className={classes.navbarTitle}>
+              <Typography
+                variant="h5"
+                color="initial"
+                className={classes.navbarTitle}
+              >
                 <Link href="/" color="inherit">
                   Portfolio
                 </Link>
               </Typography>
             </Box>
             <List>
-              <Button href="#about">
-                ABOUT
-              </Button>
-              <Button href="#skills">
-                SKILLS
-              </Button>
-              <Button href="#works">
-                WORKS
-              </Button>
-              <Button href="#contact">
-                CONTACT
-              </Button>
+              <Button href="#about">ABOUT</Button>
+              <Button href="#skills">SKILLS</Button>
+              <Button href="#works">WORKS</Button>
+              <Button href="#contact">CONTACT</Button>
             </List>
           </Container>
         </Toolbar>
       </AppBar>
     </React.Fragment>
   );
-}
+};
 
 export default Navbar;
