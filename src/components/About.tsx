3f518bf6dbd3d46a830d19b42078c38c9053384f
ITK13201 <57588603+ITK13201@users.noreply.github.com>
2021-03-26 00:17:08 +0900

    fix: a little

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,9 +7,77 @@ const useStyles = makeStyles((theme) => ({
   },
 }))
 
-const About: React.FC = () => {
+const BelongTo: React.FC = () => {
   const classes = useStyles()
 
+  return (
+    <>
+      <table className={classes.aboutTable}>
+        <tbody className={classes.aboutTable}>
+          <tr className={classes.aboutTable}>
+            <td className={classes.aboutTable}>University:</td>
+            <td className={classes.aboutTable}>Waseda University</td>
+          </tr>
+          <tr className={classes.aboutTable}>
+            <td className={classes.aboutTable}>School:</td>
+            <td className={classes.aboutTable}>
+              Fundamental Science and Engineering
+            </td>
+          </tr>
+          <tr className={classes.aboutTable}>
+            <td className={classes.aboutTable}>Department:</td>
+            <td className={classes.aboutTable}>
+              Computer Science and Engineering
+            </td>
+          </tr>
+          <tr className={classes.aboutTable}>
+            <td className={classes.aboutTable}>Grade:</td>
+            <td className={classes.aboutTable}>Bachelor #3</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  )
+}
+
+const Club: React.FC = () => {
+  const classes = useStyles()
+
+  return (
+    <>
+      <table className={classes.aboutTable}>
+        <tbody className={classes.aboutTable}>
+          <tr className={classes.aboutTable}>
+            <td className={classes.aboutTable}>Name:</td>
+            <td className={classes.aboutTable}>MIS.W</td>
+          </tr>
+          <tr className={classes.aboutTable}>
+            <td className={classes.aboutTable}>What's it?:</td>
+            <td className={classes.aboutTable}>digital creation club</td>
+          </tr>
+          <tr className={classes.aboutTable}>
+            <td className={classes.aboutTable}>Sciety:</td>
+            <td className={classes.aboutTable}>Programming</td>
+          </tr>
+          <tr className={classes.aboutTable}>
+            <td className={classes.aboutTable}>Home Page:</td>
+            <td className={classes.aboutTable}>
+              <a
+                href="https://misw.jp/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                https://misw.jp/
+              </a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  )
+}
+
+const About: React.FC = () => {
   return (
     <>
       <Box p={1}>
@@ -26,30 +94,7 @@ const About: React.FC = () => {
           </Box>
           <Box display="flex" justifyContent="center" p={1}>
             <Typography variant="body1" align="left" color="initial">
-              <table className={classes.aboutTable}>
-                <tbody className={classes.aboutTable}>
-                  <tr className={classes.aboutTable}>
-                    <td className={classes.aboutTable}>University:</td>
-                    <td className={classes.aboutTable}>Waseda University</td>
-                  </tr>
-                  <tr className={classes.aboutTable}>
-                    <td className={classes.aboutTable}>School:</td>
-                    <td className={classes.aboutTable}>
-                      Fundamental Science and Engineering
-                    </td>
-                  </tr>
-                  <tr className={classes.aboutTable}>
-                    <td className={classes.aboutTable}>Department:</td>
-                    <td className={classes.aboutTable}>
-                      Computer Science and Engineering
-                    </td>
-                  </tr>
-                  <tr className={classes.aboutTable}>
-                    <td className={classes.aboutTable}>Grade:</td>
-                    <td className={classes.aboutTable}>Bachelor #3</td>
-                  </tr>
-                </tbody>
-              </table>
+              <BelongTo />
             </Typography>
           </Box>
         </Box>
@@ -61,36 +106,7 @@ const About: React.FC = () => {
           </Box>
           <Box display="flex" justifyContent="center" p={1}>
             <Typography variant="body1" align="left" color="initial">
-              <table className={classes.aboutTable}>
-                <tbody className={classes.aboutTable}>
-                  <tr className={classes.aboutTable}>
-                    <td className={classes.aboutTable}>Name:</td>
-                    <td className={classes.aboutTable}>MIS.W</td>
-                  </tr>
-                  <tr className={classes.aboutTable}>
-                    <td className={classes.aboutTable}>What's it?:</td>
-                    <td className={classes.aboutTable}>
-                      digital creation club
-                    </td>
-                  </tr>
-                  <tr className={classes.aboutTable}>
-                    <td className={classes.aboutTable}>Sciety:</td>
-                    <td className={classes.aboutTable}>Programming</td>
-                  </tr>
-                  <tr className={classes.aboutTable}>
-                    <td className={classes.aboutTable}>Home Page:</td>
-                    <td className={classes.aboutTable}>
-                      <a
-                        href="https://misw.jp/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        https://misw.jp/
-                      </a>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
+              <Club />
             </Typography>
           </Box>
         </Box>
